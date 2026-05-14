bbd7661830185baf77bbcb160fdbdfee60887d76
rhesavarian <62584649+rhesavarian@users.noreply.github.com>
2026-05-14 18:57:45 +0700

    Add files via upload

---
+++ setup_venv.bat
@@ -0,0 +1,5 @@
+python -m venv venv
+call venv\Scripts\activate
+python -m pip install --upgrade pip
+pip install requests pandas openpyxl pyarrow jupyterlab jupyterlab-code-formatter black isort
+pause

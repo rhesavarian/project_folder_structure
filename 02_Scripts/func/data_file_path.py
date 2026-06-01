9987207afa67c0f24d0b547970b9e2f6d709d77b
rhesavarian <62584649+rhesavarian@users.noreply.github.com>
2026-06-01 18:31:30 +0700

    Update data_file_path.py

--- 02_Scripts/func/data_file_path.py
+++ 02_Scripts/func/data_file_path.py
@@ -1,12 +1,25 @@
 def get_data_path(file_path, data_type="raw"):
+    import os
     """
     Add prefix to link to data folder
     No need to use / in the initial of file_path
     data_type = "raw" or "processed", depending on the targeted folder, default to raw
+
+    It should work whether the script is run from project folder or 02_Script folder
     """
+
+    # Remove / if it's there
     if file_path.startswith("/"):
         file_path = file_path[1:]
-    if data_type == "raw":
-        return f"../01_Data/01_Raw_Data/{file_path}"
-    elif data_type == "processed":
-        return f"../01_Data/02_Processed_Data/{file_path}"
+
+    # Check if script is run in folder data
+    if {"01_Data", "02_Scripts"}.issubset(set(os.listdir())):
+        add_string = ""
+    else:
+        add_string = "../"
+
+    # Create file
+    if data_type in ["raw", "r"]:
+        return f"{add_string}01_Data/01_Raw_Data/{file_path}"
+    elif data_type in ["processed", "p"]:
+        return f"{add_string}01_Data/02_Processed_Data/{file_path}"
